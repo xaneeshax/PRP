85b811bd54c64a6891eaf7e2ff7fbba5ebab6db5
xaneeshax <64504643+xaneeshax@users.noreply.github.com>
2021-01-02 17:27:23 -0800

    Update list_comp.py

--- list_comp.py
+++ list_comp.py
@@ -8,3 +8,6 @@ print(one_to_fifty)
 one_to_fifty = [i for i in range(1,51)]
 one_to_fifty_odd = [i for i in range(1,51) if i % 2 == 1]
 one_to_fifty_odd_doubled = [i*2 for i in range(1,51) if i % 2 == 1]
+
+
+
